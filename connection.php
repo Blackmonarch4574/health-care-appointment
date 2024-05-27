1df2ec6fdd4a55aa86cdfd71cc0b535f75f9e5de
Blackmonarch4574 <162351896+Blackmonarch4574@users.noreply.github.com>
2024-05-27 22:41:45 +0530

    connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-    $database= new mysqli("localhost","root","","edoc");
+    $database= new mysqli("health-care-database.c1mqu4i64rnf.ap-south-1.rds.amazonaws.com","admin","Amrita2025","health-care-database");
     if ($database->connect_error){
         die("Connection failed:  ".$database->connect_error);
     }
