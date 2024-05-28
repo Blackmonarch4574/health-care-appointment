2ec62c16aa475d1dffc6dd3bb7151160b647b7b2
Blackmonarch4574 <162351896+Blackmonarch4574@users.noreply.github.com>
2024-05-28 10:11:24 +0530

    Delete connection.php

--- connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    $database= new mysqli("health-care-database.c1mqu4i64rnf.ap-south-1.rds.amazonaws.com","admin","Amrita2025","health-care-database");
-    if ($database->connect_error){
-        die("Connection failed:  ".$database->connect_error);
-    }
-
-?>
